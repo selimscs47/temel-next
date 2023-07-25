b24077eac4fdbfb09b749bc231ee92fac1b43e04
selimcan sağdıç <selimcanscs1907@gmail.com>
2023-07-25 18:43:28 +0300

    startt

--- pages/hakkimizda.js
+++ pages/hakkimizda.js
@@ -8,7 +8,7 @@ export default function Hakkimizda() {
         <h1 className={styles.title}>SCS Software Nedir</h1>
         <p className={styles.description}>
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exercitationem debitis eum voluptatum quasi aut molestiae obcaecati architecto nesciunt ex. Ea porro a temporibus repellat harum illum soluta magni praesentium assumenda. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum omnis beatae eveniet amet molestiae impedit, iste atque animi excepturi sequi sint doloribus est accusamus earum optio consequatur perspiciatis dolor illum.
-
+Lorem, ipsum dolor sit amet consectetur adipisicing elit. Similique, temporibus harum? Eum, inventore quo. Accusantium porro fugiat iure, repudiandae consequuntur modi placeat nemo, mollitia expedita cum, dolorum velit tenetur debitis?
         </p>
 
     </div>
