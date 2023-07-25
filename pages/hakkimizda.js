6b0f255d1fa3e8136134f53e4c8bd4bdc838a93d
selimcan sağdıç <selimcanscs1907@gmail.com>
2023-07-25 18:40:09 +0300

    start

---
+++ pages/hakkimizda.js
@@ -0,0 +1,16 @@
+import styles from '../styles/Home.module.css'
+
+import React from 'react'
+
+export default function Hakkimizda() {
+  return (
+    <div className={styles.container}>
+        <h1 className={styles.title}>SCS Software Nedir</h1>
+        <p className={styles.description}>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exercitationem debitis eum voluptatum quasi aut molestiae obcaecati architecto nesciunt ex. Ea porro a temporibus repellat harum illum soluta magni praesentium assumenda. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum omnis beatae eveniet amet molestiae impedit, iste atque animi excepturi sequi sint doloribus est accusamus earum optio consequatur perspiciatis dolor illum.
+
+        </p>
+
+    </div>
+  )
+}
---
+++ pages/index.js
@@ -0,0 +1,11 @@
+
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+    return (
+        <div className={styles.container}>
+        <h1 className={styles.title}>SCS Blog App</h1>
+        </div>
+    );
+}
+
